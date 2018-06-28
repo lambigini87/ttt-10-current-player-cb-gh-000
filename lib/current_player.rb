3b20b2f18a7a9999d083b49e4986bc262938977a
harrison tran <harrisontran@outlook.com>
2018-06-28 02:11:47 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,9 @@
 def turn_count(board)
-
+counter = 0
     board.each do |token|
-      puts "currently player #{token}""
-    end  
+      puts "currently player #{token}"
+      counter += 1
+    end
 end
+
+def
